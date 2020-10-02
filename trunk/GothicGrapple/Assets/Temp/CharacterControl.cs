1ab6a437a80df0b8f95232a44873e05d87b04ed3
Curtis Schneider <curtis.b.schneider11@gmail.com>
2020-10-01 22:16:10 -0500

    Initial Commit Boi

---
+++ trunk/GothicGrapple/Assets/Temp/CharacterControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float jumpSpeed = -10.0f;
+    private Rigidbody2D _rigidbody;
+    protected Vector2 upVector { get {return Vector2.up; } }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        MoveInput();
+    }
+
+    private void MoveInput()
+    {
+        _rigidbody.AddForce(Vector2.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(upVector * jumpSpeed);
+    }
+}
---
+++ trunk/GothicGrapple/Assets/Temp/Grapple.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+   [SerializeField] float grapplePower = 10.0f;
+    private DistanceJoint2D joint;
+    [SerializeField] private Collider2D myColliderBABY;
+    [SerializeField] Transform aimer;
+    private Vector3 grapplePoint;
+    void GrappleItBoy(Collider2D otherCollider, Vector3 point)
+    {
+        //Aww yeah
+        if (joint != null)
+        {
+            Destroy(joint);
+        }
+        grapplePoint = point;
+        joint = gameObject.AddComponent<DistanceJoint2D>();
+            joint.connectedBody = otherCollider.attachedRigidbody;
+            joint.connectedAnchor = otherCollider.transform.InverseTransformPoint(point);
+        joint.enableCollision = true;
+        
+    }
+
+    void ShootGrapple()
+    {
+        myColliderBABY.enabled = false;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -Camera.main.transform.position.z;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 checkPos = transform.position;
+        mousePos.z = 0;
+        checkPos.z = 0;
+        Vector3 dir = (mousePos - checkPos).normalized;
+        Debug.Log(dir);
+        aimer.transform.position = transform.position + dir * 2;
+        RaycastHit2D hit = (Physics2D.Raycast((Vector2)transform.position, (Vector2)dir, Mathf.Infinity));
+        if (hit.collider!=null)
+        {
+            GrappleItBoy(hit.collider, hit.point);
+        }
+        myColliderBABY.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootGrapple();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (joint != null)
+            {
+                Destroy(joint);
+            }
+        }
+        if (joint != null)
+        {
+            Vector3 dir = (grapplePoint - transform.position);
+            dir.z = 0;
+            dir.Normalize();
+            aimer.transform.position = transform.position + ((Vector3)dir * 2);
+        }
+    }
+}
