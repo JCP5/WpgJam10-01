3d05c9dd4ad270b7c0df8d4a64f24570f2e34d16
JCP5 <justen.chong@gmail.com>
2020-12-06 15:49:54 -0600

    LaserBeam and Crawler WIP

---
+++ Space Grapple/Assets/Justen/Scripts/CrawlerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlerBehaviour : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckGroundInFront()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.forward, Vector2.down, 2);
+
+        if (hit.collider == null)
+        {
+            transform.rotation += Quaternion.Euler(new Vector3(0, Mathf.Sign()
+        }
+    }
+}
