c9009d59adc8fc6384a222dfa57ca5b40c20e569
JCP5 <justen.chong@gmail.com>
2020-12-06 17:21:27 -0600

    Crawler Anims and Basic Behaviour

--- Space Grapple/Assets/Justen/Scripts/CrawlerBehaviour.cs
+++ Space Grapple/Assets/Justen/Scripts/CrawlerBehaviour.cs
@@ -4,26 +4,42 @@ using UnityEngine;
 
 public class CrawlerBehaviour : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float moveSpeed = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = transform.right * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGroundInFront();
     }
 
     void CheckGroundInFront()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.forward, Vector2.down, 2);
-
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + transform.right, Vector2.down, 2);
+        Debug.DrawLine(transform.position + transform.right, transform.position + transform.right + Vector3.down);
         if (hit.collider == null)
         {
-            transform.rotation += Quaternion.Euler(new Vector3(0, Mathf.Sign()
+            Debug.Log("Hello");
+
+            if (transform.rotation.eulerAngles.y == 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
         }
     }
 }
