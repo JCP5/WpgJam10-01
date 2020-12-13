46cba58dfb5130e7145386280374f1be1d37e109
JCP5 <justen.chong@gmail.com>
2020-12-13 17:54:07 -0600

    CrawlerGreen finished

---
+++ Space Grapple/Assets/AnimationEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventHandler : MonoBehaviour
+{
+    public Collider2D hitBox;
+
+    public void HitBoxActive()
+    {
+        hitBox.enabled = true;
+    }
+
+    public void HitBoxDeactive()
+    {
+        hitBox.enabled = false;
+    }
+}
---
+++ Space Grapple/Assets/Justen/Scripts/HitBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+        if (collision.tag == "Player")
+        {
+            CharacterControl cc = collision.GetComponent<CharacterControl>();
+            cc.Die();
+        }
+    }
+}
